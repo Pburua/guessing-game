b5ce46f3101b71ba3e60e498093ab8933f54ffd9
Pavel Burya <pburya@gmail.com>
2019-10-10 15:07:55 +0300

    init: upload task solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.lowBorder = 0;
+        this.highBorder = 0;
+    }
 
     setRange(min, max) {
-
+        this.lowBorder = min;
+        this.highBorder = max;
     }
 
     guess() {
-
+        this.lastGuess = Math.round(((this.lowBorder + this.highBorder) / 2));
+        return this.lastGuess;
     }
 
     lower() {
-
+        this.highBorder = this.lastGuess;
     }
 
     greater() {
-
+        this.lowBorder = this.lastGuess;
     }
 }
 
